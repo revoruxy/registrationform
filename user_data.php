19e58e8eb641759908a6dfca3cbefd5733ba5d80
revoruxy <revoruxy@gmail.com>
2022-05-28 21:22:30 +0300

    added code for appending values to the CSV file and use Print_r

--- user_data.php
+++ user_data.php
@@ -6,7 +6,13 @@ if(isset($_POST['submit'])){
     $gender=$_POST['gender'];
     $country=$_POST['country'];
 
-print_r($_POST);
+    print_r($_POST);
+
+    $data=[$name,$email,$dob,$gender,$country,"\n"];
+    $f=fopen("userdata.csv","a");
+    fputcsv($f,$data);
+    fclose($f);
+
     //print_r($name,$email,$dob,$gender,$country);
 }
 
